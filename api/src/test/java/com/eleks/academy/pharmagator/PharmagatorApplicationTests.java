f2aa71120203dae24b35fe06e00b92ac41adb305
sergvasylchak <hustle3942@gmail.com>
2021-10-05 15:17:06 +0300

    Initial project skeleton

---
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eleks.academy.pharmagator;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@Getter
+@SpringBootTest
+class PharmagatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
