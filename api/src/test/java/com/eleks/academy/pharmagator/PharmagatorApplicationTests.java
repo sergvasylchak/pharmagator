2e4609e3319d602e24d089baf22cc818a14b9a4b
sergvasylchak <hustle3942@gmail.com>
2021-10-05 16:00:05 +0300

    Initial project skeleton

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -1,13 +1,31 @@
 package com.eleks.academy.pharmagator;
 
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Getter
 @SpringBootTest
 class PharmagatorApplicationTests {
 
+	record Circle(int i, int j) {
+	}
+
 	@Test
 	void contextLoads() {
+		final var circle = new Circle(0, 0);
+
+		assertEquals(0, circle.i());
+	}
+
+	@Test
+	void contextLoads2() {
+		final var circle = new Circle(0, 0);
+
+		assertEquals(1, circle.i());
 	}
 
 }
